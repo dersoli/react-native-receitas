5c4e37390742721853e4c6e6f95c88d82390513e
Anderson Xavier <dersoli@gmail.com>
2023-04-05 19:11:49 -0300

    title home added

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,17 @@
-import { View, Text, StyleSheet, SafeAreaView } from 'react-native'
+import { View, Text, StyleSheet, SafeAreaView, TextInput } from 'react-native'
 import Logo from '../../components/logo'
+import { Ionicons } from '@expo/vector-icons'
 
 export default function Home(){
     return(
         <SafeAreaView style={styles.logoArea}>
             <Logo />
+            <Text style={styles.title}>Encontre a receita</Text>
+            <Text style={styles.title}>que combina com você</Text>
+
+            <View>
+
+            </View>
         </SafeAreaView>
     )
 }
@@ -16,5 +23,10 @@ const styles = StyleSheet.create({
         paddingTop: 36,
         paddingStart: 14,
         paddingEnd: 14
+    },
+    title:{
+        fontSize: 26,
+        fontWeight: 'bold',
+        color: "#0e0e0e"
     }
 })
