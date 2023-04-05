687c9756d8f1d05390368aa12c3acdf12509c0b8
Anderson Xavier <dersoli@gmail.com>
2023-04-05 18:36:56 -0300

    tab navigation added

--- App.js
+++ App.js
@@ -1,33 +1,11 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { Routes } from './src/routes'
 
 export default function App(){
   return(
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello World!</Text>
-      <Nome/>
-      <Nome name="Dersoli"/>
-    </View>
-  )
-}
-
-const styles = StyleSheet.create({
-  container:{
-    backgroundColor: 'red',
-    height: 400,
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  title:{
-    color: 'white'
-  }
-
-})
-
-function Nome({name}){
-  return(
-    <Text>
-      Olá {name}
-    </Text>
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
   )
 }
