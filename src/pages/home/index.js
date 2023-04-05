0d8b1332efd50a60a10ff736f1779e5198870819
Anderson Xavier <dersoli@gmail.com>
2023-04-05 19:50:41 -0300

    input search added

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,16 +1,33 @@
-import { View, Text, StyleSheet, SafeAreaView, TextInput } from 'react-native'
+import {useState} from 'react'
+
+import { View, Text, StyleSheet, SafeAreaView, TextInput, TouchableOpacity } from 'react-native'
 import Logo from '../../components/logo'
 import { Ionicons } from '@expo/vector-icons'
 
 export default function Home(){
+    const [inputValue, setInputValue] = useState("")
+
+
+    function handleSearch(){
+        console.log(`"voce digitou: ${inputValue}" `)
+    }
+
     return(
         <SafeAreaView style={styles.logoArea}>
             <Logo />
             <Text style={styles.title}>Encontre a receita</Text>
             <Text style={styles.title}>que combina com você</Text>
 
-            <View>
-
+            <View style={styles.form}>
+                <TextInput
+                    placeholder='Digite o nome da comida...'
+                    style={styles.input}
+                    value={inputValue}
+                    onChangeText={(text) => setInputValue(text)}
+                />
+                <TouchableOpacity onPress={handleSearch}>
+                    <Ionicons name="search" size={28} color='#4cbe6c' />
+                </TouchableOpacity>
             </View>
         </SafeAreaView>
     )
@@ -28,5 +45,24 @@ const styles = StyleSheet.create({
         fontSize: 26,
         fontWeight: 'bold',
         color: "#0e0e0e"
+    },
+    form: {
+        backgroundColor: '#fff',
+        borderWidth: 1,
+        borderColor: "#ececec",
+        width: '100%',
+        borderRadius: 8,
+        marginTop: 16,
+        marginBottom: 16,
+        paddingLeft: 8,
+        paddingRight: 8,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    input: {
+        width: '90%',
+        maxWidth: '90%',
+        height: 54
     }
 })
